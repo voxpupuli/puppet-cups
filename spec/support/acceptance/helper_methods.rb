f4230432d9f9a076a762a597093f2fadf72321bf
Romain Tartière <romain@blogreen.org>
2026-01-29 08:18:00 -1000

    Sync with modulesync
    While here, move acceptance tests helpers.

---
+++ spec/support/acceptance/helper_methods.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+def ensure_cups_is_running
+  apply_manifest('class { "cups": }', catch_failures: true)
+end
+
+def add_printers(*names)
+  names.each do |name|
+    shell("lpadmin -p #{Shellwords.escape(name)} -m drv:///sample.drv/generic.ppd -o printer-is-shared=false")
+  end
+end
+
+def add_printers_to_classes(class_members)
+  add_printers('Dummy')
+  class_members.each_key do |classname|
+    members = class_members[classname]
+    members = %w[Dummy] if members.empty?
+    members.each do |printername|
+      shell("lpadmin -p #{Shellwords.escape(printername)} -c #{Shellwords.escape(classname)}")
+    end
+    shell("lpadmin -p #{Shellwords.escape(classname)} -o printer-is-shared=false")
+  end
+  remove_queues('Dummy')
+end
+
+def remove_queues(*names)
+  names.flatten.each do |name|
+    shell("lpadmin -x #{Shellwords.escape(name)}", acceptable_exit_codes: [0, 1])
+  end
+end
+
+def purge_all_queues
+  request = '{
+    OPERATION CUPS-Get-Printers
+    GROUP operation
+    ATTR charset attributes-charset utf-8
+    ATTR language attributes-natural-language en
+    DISPLAY printer-name
+  }'
+  result = shell('ipptool -t ipp://localhost/ /dev/stdin', stdin: request, acceptable_exit_codes: [0, 1])
+  queues = result.stdout.scan(%r{printer-name \(nameWithoutLanguage\) = ([^\s"'\\,#/]+)})
+  remove_queues(queues)
+end
